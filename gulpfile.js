cc734d28b13c09f22abd5d5f78be89410ce85ebe
sladkoewk <sladkoewk.dev@gmail.com>
2019-07-02 11:52:35 +0400

    feat eslint, refactor gulpfile to modern syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-'use strict'
 const gulp = require('gulp');
 const del = require('del');
 const rename = require('gulp-rename');
@@ -13,15 +12,14 @@ const gulpIf = require('gulp-if');
 const gulplog = require('gulplog');
 const browserSync = require('browser-sync');
 const webpackStream = require('webpack-stream');
-const webpack = webpackStream.webpack;
 const named = require('vinyl-named');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-let status = process.env.STATUS || 'development';
-let isDevelopment = status === 'development';
-let isProduction = status === 'production';
+const status = process.env.STATUS || 'development';
+const isDevelopment = status === 'development';
+const isProduction = status === 'production';
 
-let path = {
+const path = {
   src: {
     base: 'src/',
     img: 'src/img/**/*.*',
@@ -29,7 +27,7 @@ let path = {
     html: ['src/index.html', 'src/pages/**/*.html'],
     style: 'src/styles/main.scss',
     jsVendor: 'src/js/vendor/**/*.*',
-    jsBase: 'src/js/*.*'
+    jsBase: 'src/js/*.*',
   },
   out: {
     root: 'public/',
@@ -37,75 +35,73 @@ let path = {
     font: 'public/font',
     html: 'public/',
     css: 'public/',
-    js: 'public/'
+    js: 'public/',
   },
   watch: {
     style: 'src/styles/**/*.*',
-    js: 'src/js/**/*.*'
-  }
-}
-let config = {
+    js: 'src/js/**/*.*',
+  },
+};
+
+const config = {
   outputCssMinName: 'bundle.min.css',
   outputCssName: 'bundle.css',
   outputJsMinName: 'bundle.min.js',
   outputJsName: 'bundle.js',
+};
+
+function clean() {
+  return del(path.out.root);
 }
 
-gulp.task('clean', function () {
-  return del(path.out.root);
-});
-
-gulp.task('asset:img', function () {
+function assetImg() {
   return gulp.src(path.src.img)
     .pipe(rename({ dirname: path.out.img }))
     .pipe(gulp.dest('.'));
-});
+}
 
-gulp.task('asset:font', function () {
+function assetFont() {
   return gulp.src(path.src.font)
     .pipe(rename({ dirname: path.out.font }))
     .pipe(gulp.dest('.'));
-});
+}
 
-gulp.task('asset:html', function () {
+function assetHtml() {
   return gulp.src(path.src.html, { base: path.src.base })
     .pipe(gulp.dest(path.out.html));
-});
+}
 
-gulp.task('assets', gulp.series(gulp.parallel('asset:img', 'asset:font', 'asset:html')));
-
-gulp.task('styles', function () {
+function styles() {
   return gulp.src(path.src.style)
     .pipe(plumber({
-      errorHandler: notify.onError(function (err) {
-        return {
-          message: "Error: <%= error.message %>",
-          title: "Styles error"
+      errorHandler: notify.onError(() => {
+        const notifyErrorTemplate = {
+          title: 'Styles error',
+          message: 'Error: <%= error.message %>',
         };
-      })
+        return notifyErrorTemplate;
+      }),
     }))
     .pipe(gulpIf(isDevelopment, sourcemaps.init()))
     .pipe(sass())
     .pipe(gulpIf(isDevelopment, concat(config.outputCssName), concat(config.outputCssMinName)))
     .pipe(autoprefixer({
-      browsers: ['last 3 versions']
+      browsers: ['last 3 versions'],
     }))
     .pipe(gulpIf(isProduction, cleanCSS({ compatibility: 'ie8' })))
     .pipe(gulpIf(isDevelopment, sourcemaps.write()))
     .pipe(gulp.dest(path.out.css))
     .pipe(browserSync.stream());
-});
+}
 
-gulp.task('webpack', function (callback) {
+function webpack(callback) {
   let firstBuildReady = false;
 
   function done(err, stats) {
     firstBuildReady = true;
-
-    if (err) { // hard error
-      return; // emit error
+    if (err) {
+      return;
     }
-
     gulplog[stats.hasErrors() ? 'error' : 'info'](stats.toString({
       colors: true,
     }));
@@ -128,7 +124,7 @@ gulp.task('webpack', function (callback) {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
           use: {
-            loader: 'babel-loader', // babel-loader?optional[]=runtime
+            loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
             },
@@ -144,54 +140,53 @@ gulp.task('webpack', function (callback) {
 
   return gulp.src([path.src.jsVendor, path.src.jsBase])
     .pipe(plumber({
-      errorHandler: notify.onError(function (err) {
-        return {
-          message: "Error: <%= error.message %>",
-          title: "Webpack error"
+      errorHandler: notify.onError(() => {
+        const notifyErrorTemplate = {
+          title: 'Webpack error',
+          message: 'Error: <%= error.message %>',
         };
-      })
+        return notifyErrorTemplate;
+      }),
     }))
     .pipe(named())
     .pipe(webpackStream(conf, null, done))
     .pipe(gulp.dest(path.out.js))
-    .on('data', function () {
+    .on('data', () => {
       if (firstBuildReady) {
         callback();
       }
     });
-});
+}
 
-// unused -> webpack task
-gulp.task('scripts', function () {
-  return gulp.src([path.src.jsVendor, path.src.jsBase])
-    .pipe(gulpIf(isDevelopment, sourcemaps.init()))
-    .pipe(gulpIf(isDevelopment, concat(config.outputJsName), concat(config.outputJsMinName)))
-    .pipe(gulpIf(isDevelopment, sourcemaps.write()))
-    .pipe(gulp.dest(path.out.js));
-});
+function watch() {
+  gulp.watch(path.src.img, gulp.series(assetImg));
+  gulp.watch(path.src.font, gulp.series(assetFont));
+  gulp.watch(path.src.html, gulp.series(assetHtml));
+  gulp.watch(path.watch.style, gulp.series(styles));
+}
 
-gulp.task('build', gulp.series('clean', gulp.parallel('styles', 'assets', 'webpack')));
-
-gulp.task('watch', function () {
-  gulp.watch(path.src.img, gulp.series('asset:img'));
-  gulp.watch(path.src.font, gulp.series('asset:font'));
-  gulp.watch(path.src.html, gulp.series('asset:html'));
-  gulp.watch(path.watch.style, gulp.series('styles'));
-  gulp.watch(path.watch.js, gulp.series('scripts'));
-});
-
-gulp.task('serve', function () {
+function serve() {
   browserSync.init({
-    server: path.out.root
+    server: path.out.root,
   });
   browserSync.watch(path.out.root).on('change', browserSync.reload);
-});
+}
 
-gulp.task('default',
-  gulp.series(
-    'build',
-    gulp.parallel(
-      'watch',
-      'serve')
-  )
-);
+exports.clean = clean;
+exports.assetImg = assetImg;
+exports.assetFont = assetFont;
+exports.assetHtml = assetHtml;
+
+const assets = gulp.parallel(assetImg, assetFont, assetHtml);
+exports.assets = assets;
+
+exports.styles = styles;
+exports.webpack = webpack;
+
+const build = gulp.series(clean, gulp.parallel(assets, styles, webpack));
+exports.build = build;
+
+exports.watch = watch;
+exports.serve = serve;
+
+exports.default = gulp.series(build, gulp.parallel(watch, serve));
